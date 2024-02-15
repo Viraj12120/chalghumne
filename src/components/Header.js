930723aab55fc965258e8fac3fa5327ca99752e1
Viraj12120 <disaleviraj17@gmail.com>
2024-02-15 16:04:41 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+	return (
+		<div>
+			<div className="grid-cols-4">
+				<div className="flex justify-between">
+					<h2 className="text-sm">Destinations</h2>
+					<h2 className="text-sm">Flights</h2>
+					<h2 className="text-sm">Trains</h2>
+					<h2 className="text-sm">Hotels</h2>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Header;
